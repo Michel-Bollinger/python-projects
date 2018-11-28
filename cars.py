56ecd5e4caf2c98b74ce4473184542311d74d1ad
Michel-Bollinger <44813254+Michel-Bollinger@users.noreply.github.com>
2018-11-28 20:43:24 +0400

    Add files via upload
    Script for creating a list of machine objects from a user-provided csv table. And together with the script is an example of a valid table.

---
+++ cars.py
@@ -0,0 +1,134 @@
+import csv
+import os.path
+import sys
+
+
+class CarBase:
+    """Базовый класс с общими атрибутами и методом"""
+
+    def __init__(self, brand, photo_file_name, carrying):
+        self.brand = brand 
+        self.photo_file_name = photo_file_name
+        self.carrying = float(carrying)
+
+    def get_photo_file_ext(self):
+        """Возвращает расширение фотографии автомобиля"""
+        _, ext = os.path.splitext(self.photo_file_name)
+        return ext
+
+
+
+class Car(CarBase):
+    """Класс легкого автомобиля"""
+    
+    def __init__(self, brand, passenger_seats_count, photo_file_name, carrying):
+        
+        self.car_type = "car"
+        
+        # если аргументы не равны пустым строкам - инициализация,
+        # в ином случае бросается исключение; в других классах аналогичный принцип
+        if passenger_seats_count and brand and carrying and photo_file_name: 
+            self.passenger_seats_count = int(passenger_seats_count)
+            super().__init__(brand, photo_file_name, carrying)
+        else:
+            raise AttributeError
+
+
+
+class Truck(CarBase):
+    """Класс грузового автомобиля"""
+
+    def __init__(self, brand, photo_file_name, body_whl, carrying):
+        
+        self.car_type = "truck"
+        
+        if body_whl and brand and photo_file_name and carrying:
+            # раскладываем строку размеров на отдельные атрибуты длины
+            # высоты и ширины, из которых вычисляется объем в методе класса
+            body_lst = body_whl.split('x')
+            self.body_length, self.body_width, self.body_height = map(float, body_lst)
+            super().__init__(brand, photo_file_name, carrying)
+        elif brand and photo_file_name and carrying:
+            self.body_length, self.body_width, self.body_height = .0, .0, .0
+            super().__init__(brand, photo_file_name, carrying)
+        else:
+            raise AttributeError
+
+
+    def get_body_volume(self):
+        """Возвращает объем грузовместимой части в кубометрах"""
+        return self.body_length * self.body_height * self.body_width
+
+
+
+class SpecMachine(CarBase):
+    """Класс спецтехника"""
+
+    def __init__(self, brand, photo_file_name, carrying, extra):
+        
+        self.car_type = "spec_machine"
+        
+        if extra and brand and carrying and photo_file_name:
+            self.extra = extra
+            super().__init__(brand, photo_file_name, carrying)
+        else:
+            raise AttributeError
+            
+
+
+
+def get_car_list(csv_filename):
+    """Возвращает из исходной csv-таблицы список объектов-автомобилей"""
+    
+    # индексирование в будущем списке, для упрощенного чтения
+    indx_car_type = 0
+    indx_brand = 1
+    indx_seats_count = 2
+    indx_photo_name = 3
+    indx_body_whl = 4
+    indx_carrying = 5
+    indx_extra = 6
+
+    # открывает файл без контекстного менеджера, т. к.
+    # это увеличает глубину отступов
+    csv_fd = open(csv_filename)
+    reader = csv.reader(csv_fd, delimiter=';')
+    
+    #пропускаем заголовок csv
+    next(reader)
+    
+    # будущий список объектов
+    car_list = []
+    
+    # пробегаемся по созданному csv-объекту
+    for row in reader:
+        car = None
+        
+        try:
+            # проверяем каждую строку на принадлежность на соотвествие имеющимся классам
+            if row[indx_car_type] == "car":
+                car = Car(row[indx_brand], row[indx_seats_count], row[indx_photo_name], row[indx_carrying])
+            
+            elif row[indx_car_type] == "truck":
+                car = Truck(row[indx_brand], row[indx_photo_name], row[indx_body_whl], row[indx_carrying])
+            
+            elif row[indx_car_type] == "spec_machine":
+                car = SpecMachine(row[indx_brand], row[indx_photo_name], row[indx_carrying], row[indx_extra])
+            
+            # в случае пустой строки пропускаем ее
+            else:
+                continue
+            
+            car_list.append(car)
+        
+        # пропуск строки, если числовые данные некорректны или 
+        # инициализатор класса поднял AttributeError из-за наличия атрибутов в виде пустых строк
+        except (IndexError, AttributeError, ValueError):
+            continue
+    
+    csv_fd.close()
+
+    return car_list
+
+if __name__ == '__main__':
+    get_car_list(sys.argv[1])
